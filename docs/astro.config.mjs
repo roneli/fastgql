18d8fd7f36fe50dac15f31db363c699f45accfef
Ron <38083777+roneli@users.noreply.github.com>
2023-12-30 21:44:58 +0200

    fix base url astro

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://roneli.github.io',
+    base: "/fastgql",
     integrations: [
         starlight({
             title: '',
@@ -18,33 +19,33 @@ export default defineConfig({
                 {
                     label: 'Getting Started',
                     items: [
-                        {label: 'Introduction', link: '/start/intro'},
-                        {label: 'Setup', link: '/start/setup'},
+                        {label: 'Introduction', link: '/fastgql/start/intro'},
+                        {label: 'Setup', link: '/fastgql/start/setup'},
                     ]
                 },
                 {
                     label: 'Queries',
                     items: [
-                        {label: 'Querying', link: '/queries/queries'},
-                        {label: 'Filtering', link: '/queries/filtering'},
-                        {label: 'Ordering', link: '/queries/ordering'},
-                        {label: 'Pagination', link: '/queries/pagination'},
-                        {label: 'Aggregation', link: '/queries/aggregation'},
+                        {label: 'Querying', link: '/fastgql/queries/queries'},
+                        {label: 'Filtering', link: '/fastgql/queries/filtering'},
+                        {label: 'Ordering', link: '/fastgql/queries/ordering'},
+                        {label: 'Pagination', link: '/fastgql/queries/pagination'},
+                        {label: 'Aggregation', link: '/fastgql/queries/aggregation'},
                         ]
                 },
                 {
                     label: 'Mutations',
                     items: [
-                        {label: 'Insert', link: '/mutations/insert'},
-                        {label: 'Update', link: '/mutations/update'},
-                        {label: 'Delete', link: '/mutations/delete'},
+                        {label: 'Insert', link: '/fastgql/mutations/insert'},
+                        {label: 'Update', link: '/fastgql/mutations/update'},
+                        {label: 'Delete', link: '/fastgql/mutations/delete'},
                     ]
                 },
                 {
                     label: 'Schema',
                     items: [
-                        {label: 'Directives', link: '/schema/directives'},
-                        {label: 'Schema', link: '/schema/fastgql_schema_fragment'},
+                        {label: 'Directives', link: '/fastgql/schema/directives'},
+                        {label: 'Schema', link: '/fastgql/schema/fastgql_schema_fragment'},
                     ],
                 },
                 {
