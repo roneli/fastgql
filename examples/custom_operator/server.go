10aaf7ad58a284b462b29d02b6b512e8972292a0
Ron <38083777+roneli@users.noreply.github.com>
2024-03-17 00:46:27 +0200

    check custom operator (#55)
    * check custom operator

---
+++ examples/custom_operator/server.go
@@ -0,0 +1,65 @@
+//go:generate go run github.com/roneli/fastgql generate -c gqlgen.yml -f
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+
+	"github.com/doug-martin/goqu/v9"
+	"github.com/doug-martin/goqu/v9/exp"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/roneli/fastgql/examples/custom_operator/graph"
+	"github.com/roneli/fastgql/examples/custom_operator/graph/generated"
+	"github.com/roneli/fastgql/pkg/execution/builders"
+	"github.com/roneli/fastgql/pkg/log/adapters"
+	"github.com/rs/zerolog/log"
+)
+
+const defaultPort = "8081"
+
+const defaultPGConnection = "postgresql://localhost/movies?user=postgres&password=password"
+
+func MyCustomOperator(table exp.AliasedExpression, key string, value interface{}) goqu.Expression {
+	return goqu.L("1 = 1")
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+	pgConnectionString := os.Getenv("PG_CONN_STR")
+	if pgConnectionString == "" {
+		pgConnectionString = defaultPGConnection
+	}
+
+	pool, err := pgxpool.New(context.Background(), pgConnectionString)
+	if err != nil {
+		panic(err)
+	}
+	resolver := &graph.Resolver{}
+	executableSchema := generated.NewExecutableSchema(generated.Config{Resolvers: resolver})
+	// Set configuration
+	cfg := &builders.Config{
+		Schema: executableSchema.Schema(),
+		Logger: adapters.NewZerologAdapter(log.Logger),
+		CustomOperators: map[string]builders.Operator{
+			"myCustomOperator": MyCustomOperator,
+		},
+	}
+	resolver.Cfg = cfg
+	resolver.Executor = pool
+	srv := handler.NewDefaultServer(executableSchema)
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal().Err(err)
+	}
+}
